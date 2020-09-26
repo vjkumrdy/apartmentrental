35c7174e33bb7206c9c7e0267691c816784c078b
vjkumrdy <vjkumrdy@gmail.com>
2020-09-26 14:26:56 -0400

    This is a commit for login,splash and main activity

---
+++ app/src/main/java/com/findinganapartment/SplashScreenActivity.java
@@ -0,0 +1,39 @@
+package com.findinganapartment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+        final int ScreenDisplay = 3000;
+        Thread t1=new Thread(){
+            int wait1=0;
+            public void run(){
+                try{
+                    while(wait1<=ScreenDisplay )
+                    {
+                        sleep(100);
+                        wait1+=100;
+                    }
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+                finally{
+                    Intent i= new Intent(SplashScreenActivity.this, LoginActivity.class);
+                    startActivity(i);
+                    finish();
+
+                }
+            }
+        };
+        t1.start();
+    }
+}
