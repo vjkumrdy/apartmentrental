9113e9642bbc2a01bc4eeb05ec95e04e434025cc
vjkumrdy <vjkumrdy@gmail.com>
2020-10-22 22:58:29 -0400

    booking activity added

---
+++ app/src/main/java/com/findinganapartment/activities/BookingActivity.java
@@ -0,0 +1,87 @@
+package com.findinganapartment.activities;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.findinganapartment.R;
+import com.findinganapartment.api.ApiService;
+import com.findinganapartment.api.RetroClient;
+import com.findinganapartment.models.ResponseData;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class BookingActivity extends AppCompatActivity {
+    TextView property_id,property_name;
+    Button btn_book_now;
+    EditText et_msg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking);
+
+
+        getSupportActionBar().setTitle("Request Property");
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        property_id=(TextView)findViewById(R.id.property_id);
+        property_name=(TextView)findViewById(R.id.property_name);
+        et_msg=(EditText)findViewById(R.id.et_msg);
+
+        property_id.setText("Property Id  :"+getIntent().getStringExtra("pid"));
+        property_name.setText("Email  :"+getIntent().getStringExtra("uname"));
+
+        btn_book_now=(Button)findViewById(R.id.btn_book_now);
+        btn_book_now.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                serverData();
+            }
+        });
+    }
+
+    public  void serverData() {
+        ApiService apiService = RetroClient.getRetrofitInstance().create(ApiService.class);
+        Call<ResponseData> call = apiService.booknow(getIntent().getStringExtra("pid"),getIntent().getStringExtra("uname"),et_msg.getText().toString());
+        call.enqueue(new Callback<ResponseData>() {
+            @Override
+            public void onResponse(Call<ResponseData> call, Response<ResponseData> response) {
+                if (response.body().message.equals("true")) {
+                    Toast.makeText(BookingActivity.this, response.body().message, Toast.LENGTH_LONG).show();
+                    Log.i("msg", "" + response.body().message);
+                    finish();
+                } else {
+                    Toast.makeText(BookingActivity.this, response.body().message, Toast.LENGTH_LONG).show();
+                    finish();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseData> call, Throwable t) {
+                Log.d("TAG","Response = "+t.toString());
+            }
+        });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
