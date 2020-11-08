cf6a7ac3ebcee5bde9f5376bfc161a942b6a8108
SaiTeja <yatamsaiteja@gmail.com>
2020-11-07 21:52:24 -0500

    changes made in the code

--- app/src/main/java/com/findinganapartment/activities/SlashScreenActivity.java
+++ app/src/main/java/com/findinganapartment/activities/SlashScreenActivity.java
@@ -21,7 +21,7 @@ public class SlashScreenActivity extends AppCompatActivity {
                 try{
                     while(wait1<=ScreenDisplay )
                     {
-                        sleep(200);
+                        sleep(100);
                         wait1+=100;
                     }
                 }
